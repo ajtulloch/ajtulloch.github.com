a5ebf1ec1959f00e538c19556286c0564aaa96bd
Andrew Tulloch <ajtulloch@gmail.com>
2012-03-18 12:27:46 +1100

    Updates

--- js/mixpanel_test.js
+++ js/mixpanel_test.js
@@ -1,1 +1,5 @@
-mixpanel.track('Page Viewed', {'page name' : document.title, 'url' : window.location.pathname});
+mixpanel.track('Page Viewed', 
+                { 'page name' : document.title, 
+                  'url' : window.location.pathname, 
+                  'referrer': window.location.referrer
+                });

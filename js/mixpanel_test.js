7aa2dd0e28956629295970a476269f2f1410595f
Andrew Tulloch <ajtulloch@gmail.com>
2012-03-18 12:29:51 +1100

    fixed mixpanel

--- js/mixpanel_test.js
+++ js/mixpanel_test.js
@@ -1,5 +1,5 @@
 mixpanel.track('Page Viewed', 
                 { 'page name' : document.title, 
                   'url' : window.location.pathname, 
-                  'referrer': window.location.referrer
+                  'referrer': document.referrer
                 });
